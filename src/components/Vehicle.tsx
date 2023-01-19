60dba4ae7a458896c9e08308f311d585746498c7
Nicolas Mazieres <111435820+NicolasMazieres@users.noreply.github.com>
2023-01-19 19:54:57 +0100

    Added vehicle component

---
+++ src/components/Vehicle.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+
+function Vehicle() {
+    <div>
+        <label htmlFor="vehicle-select">Choose a pet:</label>
+        <select name="vehicles" id="vehicle-select">
+            <option value="">--Please choose an option--</option>
+            <option value="bike">Vélo</option>
+            <option value="ebike">Vélo électrique</option>
+            <option value="carE85">Voiture (Superéthanol E85)</option>
+            <option value="cardiesel">Voiture (Diesel)</option>
+            <option value="carpetrol">Voiture (Essence)</option>
+            <option value="carGPL">Voiture (GPL)</option>
+            <option value="motorcycle">Moto/Scooter</option>
+            <option value="escooter">Scooter électrique</option>
+            <option value="ecar">Voiture électrique</option>
+            <option value="hybridcar">Voiture hybride</option>
+        </select>
+    </div>
+}
+
+export default Vehicle;
