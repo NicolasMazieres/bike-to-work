274e26bfd747b04168142b4fbb7df92778f39c3f
Nicolas Mazieres <111435820+NicolasMazieres@users.noreply.github.com>
2023-02-12 00:04:03 +0100

    Update chart component + estimation results

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,32 +1,42 @@
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend, ResponsiveContainer } from 'recharts';
 
 interface Props {
-    oldEmissions: number;
-    newEmissions: number;
+    data1: [number, number];
+    data2: [number, number];
+    unit1: string;
+    unit2: string;
+    legend1: string;
+    legend2: string;
 }
 
 function Chart(props: Props) {
     const data = [
         {
-            name: 'Anciennes émissions',
-            co2: props.oldEmissions,
+            name: "Avant",
+            value1: props.data1[0],
+            value2: props.data2[0],
         },
         {
-            name: 'Nouvelles émissions',
-            co2: props.newEmissions,
+            name: "Après",
+            value1: props.data1[1],
+            value2: props.data2[1],
         }
     ];
 
     return (
         <div className="barchart-container">
-            <h2>Bar Chart</h2>
             <ResponsiveContainer width="50%" aspect={2} >
-                <BarChart data={data} margin={{top: 20, right: 100, left: 100, bottom: 5,}} barSize={100}>
+                <BarChart data={data} margin={{ top: 20, right: 100, left: 100, bottom: 5, }} barSize={100}>
                     <XAxis dataKey="name" scale="point" padding={{ left: 100, right: 100 }} />
-                    <YAxis dataKey="co2" unit=" t/an" />
-                    <Tooltip />
+                    <YAxis dataKey="value1" unit={props.unit1} yAxisId="left" />
+                    <YAxis dataKey="value2" unit={props.unit2} yAxisId="right" orientation="right" />
                     <CartesianGrid strokeDasharray="3 3" />
-                    <Bar dataKey="co2" fill="#8884d8" background={{ fill: '#eee' }} />
+                    <Legend payload={[
+                        { value: props.legend1, type: "rect", color: "#8884d8" },
+                        { value: props.legend2, type: "rect", color: "#1884d8" }
+                    ]} />
+                    <Bar dataKey="value1" yAxisId="left" fill="#8884d8" background={{ fill: '#eee' }} />
+                    <Bar dataKey="value2" yAxisId="right" fill="#1884d8" background={{ fill: '#eee' }} />
                 </BarChart>
             </ResponsiveContainer>
         </div>
--- src/components/EstimateEmissions.tsx
+++ src/components/EstimateEmissions.tsx
@@ -11,32 +11,61 @@ interface Props {
 function EstimateEmissions(props: Props) {
     const [oldEmissions, setOldEmissions] = useState<number>(0);
     const [newEmissions, setNewEmissions] = useState<number>(0);
+    const [oldCost, setOldCost] = useState<number>(0);
+    const [newCost, setNewCost] = useState<number>(0);
+
 
     const weeksWorkedPerYear = 45.6;
 
     const oldDistance = props.oldVehicleData.distance;
     const oldEmissionFactor = props.oldVehicleData.emissionFactor;
     const oldDaysPerWeek = props.oldVehicleData.daysPerWeek;
+    const oldConsommation = props.oldVehicleData.consommation / 100;
+    const oldPrice = props.oldVehicleData.price;
 
     const newDistance = props.newVehicleData.distance;
     const newEmissionFactor = props.newVehicleData.emissionFactor;
     const newDaysPerWeek = props.newVehicleData.daysPerWeek;
+    const newConsommation = props.newVehicleData.consommation / 100;
+    const newPrice = props.newVehicleData.price;
 
     function estimateEmissions() {
-        setOldEmissions(oldDistance * oldEmissionFactor * oldDaysPerWeek * weeksWorkedPerYear/1000);
+        setOldEmissions(oldDistance * oldEmissionFactor * oldDaysPerWeek * weeksWorkedPerYear / 1000);
         if (props.isBoxChecked && (newDaysPerWeek < oldDaysPerWeek)) {
-            setNewEmissions((newDistance * newEmissionFactor * newDaysPerWeek * weeksWorkedPerYear + oldDistance * oldEmissionFactor * (oldDaysPerWeek - newDaysPerWeek) * weeksWorkedPerYear)/1000);
+            setNewEmissions((newDistance * newEmissionFactor * newDaysPerWeek * weeksWorkedPerYear + oldDistance * oldEmissionFactor * (oldDaysPerWeek - newDaysPerWeek) * weeksWorkedPerYear) / 1000);
         }
         else {
-            setNewEmissions(newDistance * newEmissionFactor * newDaysPerWeek * weeksWorkedPerYear/1000);
+            setNewEmissions(newDistance * newEmissionFactor * newDaysPerWeek * weeksWorkedPerYear / 1000);
         }
+    }
+
+    function estimatePrices() {
+        setOldCost(oldDistance * oldPrice * oldConsommation * oldDaysPerWeek * weeksWorkedPerYear);
+        if (props.isBoxChecked && (newDaysPerWeek < oldDaysPerWeek)) {
+            setNewCost(newDistance * newPrice * newConsommation * newDaysPerWeek * weeksWorkedPerYear + oldDistance * oldPrice * oldConsommation * (oldDaysPerWeek - newDaysPerWeek) * weeksWorkedPerYear);
+        }
+        else {
+            setNewCost(newDistance * newPrice * newConsommation * newDaysPerWeek * weeksWorkedPerYear);
+        }
+    }
+
+    function handleClick() {
+        estimateEmissions();
+        estimatePrices();
     }
 
     return (
         <div>
-            <button onClick={estimateEmissions}>Estimer les émissions</button>
+            <button onClick={handleClick}>Faire le comparatif</button>
 
-            <Chart oldEmissions={oldEmissions} newEmissions={newEmissions}/>
+            <Chart
+                data1={[oldEmissions, newEmissions]}
+                unit1=" t/an"
+                legend1="Emissions annuelles de CO2"
+                data2={[oldCost, newCost]}
+                unit2=" €/an"
+                legend2="Coût annuel en €"
+            />
         </div>
     )
 }
