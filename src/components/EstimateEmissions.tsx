d7972b391e121aa1200e418fed8908771a303a52
Nicolas Mazieres <111435820+NicolasMazieres@users.noreply.github.com>
2023-03-09 23:43:47 +0100

    CSS update

--- src/components/EstimateEmissions.tsx
+++ src/components/EstimateEmissions.tsx
@@ -81,11 +81,11 @@ function EstimateEmissions(props: Props) {
             <div className="svg-container">
                 <div className="svg">
                     <img src=".\images\emissions.svg" alt="CO2 emissions" height={80} />
-                    <p>{savedResults.co2Saved >= 0 ? ("Moins ") : ("Plus ") } <strong>{Math.abs(savedResults.co2Saved).toFixed(2)} tonnes de CO2 </strong> émises par an</p>
+                    <p>{savedResults.co2Saved >= 0 ? ("Moins ") : ("Plus ") } <strong className="co2-strong">{Math.abs(savedResults.co2Saved).toFixed(2)} tonnes de CO2</strong> émises par an</p>
                 </div>
                 <div className="svg euro-svg">
                     <img src=".\images\euro.svg" alt="Euro" height={80} />
-                    <p>{savedResults.moneySaved >= 0 ? ("Gain de ") : ("Perte de ") } <strong>{Math.abs(savedResults.moneySaved).toFixed(2)} € </strong>par an</p>
+                    <p>{savedResults.moneySaved >= 0 ? ("Gain de ") : ("Perte de ") } <strong className="money-strong">{Math.abs(savedResults.moneySaved).toFixed(2)} €</strong> par an</p>
                 </div>
             </div>
             <Chart
